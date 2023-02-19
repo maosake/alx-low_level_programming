8ead9b1246d52ef3c230552ce16a568cdf9f374f
maosake <maosake>
2023-02-19 02:58:41 -0800

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-main - Entry int main(void)
+main - Entry
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
