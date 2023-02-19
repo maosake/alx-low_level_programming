432abd7c1ddd406b174c9cb82505daec1bcbb39d
maosake <maosake>
2023-02-19 00:30:20 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main - size of various types on computer
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void) /* function to print size of types on the computer*/
+int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of a int: %d byte(s)\n", sizeof(int));
