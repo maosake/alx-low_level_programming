747a2f5ccd6c028e67aef40a181cab062addb553
maosake <maosake>
2023-03-02 18:45:07 -0800

    all

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,14 +1,13 @@
-#include "main.h"                                                                                                                  
-#include <unistd.h>                                                                                                                     
-                                                                                                                                        
-/**                                                                                                                                     
- * _putchar - writes the character c to stdout                                                                                          
- * @c: The character to print                                                                                                           
- *                                                                                                                                      
- * Return: On success 1.                                                                                                                
- * On error, -1 is returned, and errno is set appropriately.                                                                            
- */                                                                                                                                     
-int _putchar(char c)                                                                                                                    
-{                                                                                                                                       
-        return (write(1, &c, 1));                                                                                                       
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to st
+ * @c: THe character to print
+ * Return: 1 on success
+ * on erro, -1 and 
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
