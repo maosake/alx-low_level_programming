c6c5aed094975a8b826b645b85441f84fe815572
maosake <maosake>
2023-03-04 10:41:31 -0800

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,18 +6,20 @@
  */
 char *leet(char *n)
 {
-	int indx1 = 0, indx2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	while (str[indx1])
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (indx2 = 0; indx2 <= 7; indx2++)
+		for (j = 0; j < 10; j++)
 		{
-			if (str[indx1] == leet[indx2] || str[indx1] - 32 == leet[indx2])
-				str[indx1] = indx2 + '0';
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
 		}
-		indx1++;
 	}
-	return (str);
+	return (n);
 }
 
