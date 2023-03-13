23033be342f05fcb8876f424576acaf03147e3fd
maosake <maosake>
2023-03-13 02:40:51 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,17 @@ int main(int argc, char *argv[])
 	int a, b;
 	int result;
 
-	if (argc != 3 && argc > 3)
+	if (argc == 3)
 	{
-		printf("Error\n");
-		return (1);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		result = a * b;
+
+		printf("%d/n", result);
+
+		return (0);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	result = a * b;
-
-	printf("The result is: %d\n", result);
-
-	return (0);
+	printf("Error\n");
+	return (1);
 }
+	
