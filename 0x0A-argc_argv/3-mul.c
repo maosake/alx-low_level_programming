8638cebca57fff4fd8897d4a765f9384c9d12082
maosake <maosake>
2023-03-13 02:25:03 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - prints a multiplication of two numbers
  * @argc: number of arguments
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	a = _atoi(argv[1]);
-	b = _atoi(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 	result = a * b;
 
 	printf("The result is: %d\n", result);
