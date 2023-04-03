bce42d933fddd7667c7284bc2feb82eb6e463104
maosake <maosaken6@gmail.com>
2023-04-03 11:37:16 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	const list_t *ptr = null;
-	ptr = h;
+	const list_t *ptr = NULL;
 
+	ptr = h;
 	while (ptr)
 	{
 		if (ptr == NULL)
 			printf("[0] (nil)");
-		else 
-			printf("[%u] %s", ptr->len, ptr->str);
+		else
+			printf("[%lu] %s", ptr->len, ptr->str);
 		ptr = ptr->link;
 		count++;
 	}
