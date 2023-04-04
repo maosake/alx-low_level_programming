f10396e6cb228a329caf74160f6d15c9c724a44f
maosake <maosaken6@gmail.com>
2023-04-03 23:23:27 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,8 +12,8 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	
-	while (h)
+
+	while (h != NULL)
 	{
 		if (h->str  == NULL)
 			printf("[0] (nil)");
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-/*#define const list_t;*/
+
+#include <stddef.h>
 typedef struct node
 {
 	char *str;
