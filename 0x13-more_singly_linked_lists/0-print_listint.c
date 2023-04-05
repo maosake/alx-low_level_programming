ce94d71d827867efac5bfc3723daf2014309cabf
maosake <maosaken6@gmail.com>
2023-04-04 20:12:47 -0700

    add task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,12 +14,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (h->n == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] \n", h->n)
-		h = h->next;
+		printf("%d\n", h->n);
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
