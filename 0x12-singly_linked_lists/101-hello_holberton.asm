ea6f006ab1492801b16c67522a68984eaf84cf6c
maosake <maosaken6@gmail.com>
2023-04-05 13:11:59 -0700

    task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,11 @@
 global main
 extern    printf
 main:
-	  mov   rdi, format
-	mov rsi, message
+	  mov   edi, message
+
 	  xor   eax, eax
 	  call  printf
 	  mov   eax, 0
 	  ret
 message: db `Hello, Holberton\n`,0
-format: db "%s\n",0
 
