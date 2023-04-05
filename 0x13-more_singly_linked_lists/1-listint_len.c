d11aedaf0afce3a86ef138ab5ac638d293c29ded
maosake <maosaken6@gmail.com>
2023-04-05 13:44:11 -0700

    task0

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,11 +7,11 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
